01eacc3b190f0003f11d9826841d606e7e05d318
Jana Elbrecht <j.s.elbrecht@students.uu.nl>
2019-01-18 13:57:29 +0100

    Plots in R

--- Ukraine.R
+++ Ukraine.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(RColorBrewer)
+library(ggthemes)
 install.packages("RColorBrewer")
 
 #Conflict Ukraine as df
@@ -13,10 +15,18 @@ Conflicts_Ukraine <- transform(Conflicts_Ukraine,
                                active.year = as.logical(active.year),
                                type.of.violence = as.factor(type.of.violence),
                                side.a.name = as.factor(side.a.name),
-                               side.b.name = as.factor(side.b.name)
+                               side.b.name = as.factor(side.b.name),
+                               conflict.name = as.factor(conflict.name)
                                )
 
+#statistics
 print(mean(Conflicts_Ukraine$total.deaths))
+print(sum(Conflicts_Ukraine$total.deaths))
+
+#frequency table
+table_by_actors <- table(Conflicts_Ukraine$side.a.name, Conflicts_Ukraine$side.b.name)
+table_by_actors
+
 
 #plot of deaths by date 
 ggplot(data = Conflicts_Ukraine) +
@@ -27,7 +37,10 @@ Datesame <- Conflicts_Ukraine$early.estimate.of.date == Conflicts_Ukraine$late.e
 Conflicts_Ukraine$Datesame <- Datesame
 
 ggplot(data = Conflicts_Ukraine) +
-  geom_point(aes(x=early.estimate.of.date, y=late.estimate.of.date, color=Datesame))
+  geom_point(aes(x=early.estimate.of.date, y=late.estimate.of.date, color=Datesame)) +
+  labs (x='Earliest estimate of date', y='Latest estimate of date', color='Dates are the same') +
+  ggtitle('Accuracy of estimated dates of violent events') +
+  theme(legend.text=element_text(size=8), legend.title = element_text(size = 8), legend.position = 'bottom')
 
 #determining whether something happened before or after elections
 date_election <- "2014-05-25"
@@ -43,7 +56,6 @@ levels(Conflicts_Ukraine$side.a.name) <- list('Government'="Government of Ukrain
 levels(Conflicts_Ukraine$side.b.name)
 
 
-
 library(RColorBrewer)
 
 #plotting who causes conflicts before and after elections 
@@ -54,5 +66,47 @@ ggplot(data = Conflicts_Ukraine) +
   labs(y='percentage of conflicts', x='side A', fill='side B') +
   theme(legend.text=element_text(size=8), axis.text.x = element_text(angle = 90, hjust = 1))
 
-Conflicts_Ukraine$month <- as.character(Conflicts_Ukraine$early.estimate.of.date, format="%m-%Y")
+#plotting the type of conflict before and after elections
+ggplot(data = Conflicts_Ukraine) +
+  geom_bar(aes(x=side.b.name, fill=conflict.name), stat="count", position='fill') +
+  scale_fill_brewer(palette="Set2") +
+  facet_grid(.~after_election) + 
+  labs(y='percentage of conflicts', x='Adversary of the government', fill='Conflict') +
+  theme(legend.text=element_text(size=8), axis.text.x = element_text(angle = 90, hjust = 1))
+
+#per month
+Conflicts_Ukraine_months <- Conflicts_Ukraine %>%
+  mutate(
+    month = format(early.estimate.of.date, format='%Y-%m-01'),
+    month = ifelse(month == '2014-05-01' & after_election==T, '2014-05-15', month),
+    month = as.Date(month)
+  )
+
+mean_deaths <- Conflicts_Ukraine_months %>%
+  group_by(month) %>%
+  summarise(mean(total.deaths), na.rm=TRUE)
+
+mean_deaths <- transform(mean_deaths,
+                         Mean_deaths = mean(total.deaths)
+                         )
+mean_deaths$mean_total_deaths = mean_deaths$`mean(total.deaths)`
+
+
+Conflicts_Ukraine$month <- as.character(Conflicts_Ukraine$early.estimate.of.date, format="%Y-%m")
 Conflicts_Ukraine$monthfactor <- as.factor(Conflicts_Ukraine$month)
+
+ggplot() +
+  geom_bar(data = Conflicts_Ukraine_months, aes(x=month), fill='darkblue', stat="count") +
+  geom_vline(xintercept=date_election, color='red') +
+  labs(y='Number of violent events', x='Month') +
+  ggtitle('Violence in Ukraine before and after the 2014 elections') +
+  theme_classic() +
+  theme(legend.text=element_text(size=8), axis.text.x = element_text(angle = 90, hjust = 1))
+
+ggplot() +
+  geom_area(data = mean_deaths, aes(x=month, y=mean_total_deaths)) +
+  geom_vline(xintercept=date_election, color='red') +
+  ggtitle('Fatality of violent events in Ukraine') + 
+  labs(y='Mean number of deaths per violent event', x='Month') +
+  theme(legend.text=element_text(size=8), axis.text.x = element_text(angle = 90, hjust = 1))
+
